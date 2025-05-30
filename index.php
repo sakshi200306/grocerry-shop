79553ce049fdecb6d444a64260eac9fa277a9e37
= <=>
2025-05-29 22:54:34 -0700

    some changes

--- index.php
+++ index.php
@@ -54,14 +54,14 @@ https://templatemo.com/tm-591-villa-agency
             <!-- <li><i class="fa fa-map"></i> Sunny Isles Beach, FL 33160</li> -->
           </ul>
         </div>
-        <div class="col-lg-4 col-md-4">
+        <!-- <div class="col-lg-4 col-md-4">
           <ul class="social-links">
             <li><a href="#"><i class="fab fa-facebook"></i></a></li>
             <li><a href="https://x.com/minthu" target="_blank"><i class="fab fa-twitter"></i></a></li>
             <li><a href="#"><i class="fab fa-linkedin"></i></a></li>
             <li><a href="#"><i class="fab fa-instagram"></i></a></li>
           </ul>
-        </div>
+        </div> -->
       </div>
     </div>
   </div>
