37b75ca2014c404f62a432e80053105f1368fe3f
sakshipawar4811 <sakship4811kka@gmail.com>
2025-05-31 11:01:38 +0530

    some changes

---
+++ about.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+
+    <title>Grocerry Shop</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="assets/css/fontawesome.css">
+    <link rel="stylesheet" href="assets/css/templatemo-villa-agency.css">
+    <link rel="stylesheet" href="assets/css/owl.css">
+    <link rel="stylesheet" href="assets/css/animate.css">
+    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+    <!--
+
+TemplateMo 591 villa agency
+
+https://templatemo.com/tm-591-villa-agency
+
+-->
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+</head>
+
+<body>
+
+</body>
+
+<?php
+include "header.php";
+?>
+<div id="mainCarousel" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner">
+        <!-- First Slide (Active) -->
+        <div class="carousel-item active">
+            <img src="grocerry1.jpg" class="d-block w-100" style="height: 600px;" alt="Slide 1">
+            <div class="carousel-caption d-none d-md-block">
+                <!-- <h1 style="color: black;">WEL-COME TO OUR</h1> -->
+
+                <!-- <h1 style="color: black;">MORE SUPER MARKET</h1> -->
+                <!-- <p style="color: black;">Hurry! Get the Best products</p> -->
+            </div>
+        </div>
+
+    </div>
+</div>
+
+
+<!-- header pageee -->
+
+
+
+<div class="container">
+    <div class="container-fluid">
+
+
+        <h2 style="color: black; font-size: 45px; text-align:center;">Welcome To MORE Super Market</h2>
+        <br>
+        <p style="font-size: 35px;  text-align:center;">- your trusted neighborhood grocerry store ! <br> We have been serving our community since (1989), with fresh fruits , vegetables , dairy , grains , and daily essentials at affordable prices . </p>
+
+    </div>
+</div>
+<!-- middle middle section -->
+<br><br>
+<div class="container">
+    <div class="container-fluid">
+        <div class="row">
+            <h3 style="color: black; font-size: 45px; text-align:center;">OUR GOAL IS TO PROVIDE</h3>
+
+            <div class="col-sm-4"> <br><br>
+                <img style="height: 250px; width: 400px;" src="quality.jpg" alt=""><br>
+                <br>
+                <h3 style="text-align: center;">Quality Products</h3>
+            </div>
+            <div class="col-sm-4"> <br><br>
+                <img style="height: 250px; width: 400px;" src="friendly.jpg" alt=""><br>
+                <br>
+                <h3 style="text-align: center;">Friendly Services </h3>
+            </div>
+            <div class="col-sm-4"> <br><br>
+                <img style="height: 250px; width: 400px;" src="clean.jpg" alt=""><br>
+                <br>
+                <h3 style="text-align: center;">Clean environment</h3>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<br><br>
+<!-- last section -->
+ <div class="container">
+    <div class="container-fluid">
+        <div class="row">
+            <h3 style="color: black; font-size: 45px; text-align:center;">WE ALSO OFFER ----</h3>
+
+            <div class="col-sm-4"> <br><br>
+            <h3 style="text-align: center;">Home Delivery</h3><br>
+                <img style="height: 250px; width: 400px;" src="home.jpg" alt="">
+                
+            </div>
+            <div class="col-sm-4"> <br><br>
+             <h3 style="text-align: center;">Online Delivery </h3><br>
+                <img style="height: 250px; width: 400px;" src="online.jpg" alt="">
+               
+            </div>
+            <div class="col-sm-4"> <br>
+            <h3 style="text-align: center;">Special discounts for regular customer</h3>
+                <img style="height: 270px; width: 400px;" src="offer.jpg" alt="">
+                
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<?php
+include "footer.php";
+?>
