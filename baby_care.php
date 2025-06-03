f4d06a82b99ff3278326a273704358861928537a
sakshipawar4811 <sakship4811kka@gmail.com>
2025-06-03 10:25:39 +0530

    push

---
+++ baby_care.php
@@ -0,0 +1,56 @@
+<?php
+include "header.php";
+?>
+<br><br>
+<h1 style="text-align: center;">Baby Care</h1>
+<div class="container">
+    <div class="contaiber-fluid">
+        <div class="row">
+            <div class="col-sm-3">
+                <img src="baby.jfif" alt="">
+                <h2>WIPES</h2>
+            </div>
+
+            <div class="col-sm-3">
+                <img src="food1.jfif" alt="">
+                <h2>FOODS</h2>
+            </div>
+
+           <div class="col-sm-3">
+                <img src="g.1.jfif" alt="">
+                <h2>GROOMING</h2>
+            </div>
+                       <div class="col-sm-3">
+                <img src="skin7.jfif" alt="">
+                <h2>SKIN CARE</h2>
+            </div>
+    
+            </div>
+                       <div class="col-sm-3">
+                <img src="skin7.jfif" alt="">
+                <h2>SKIN CARE</h2>
+            </div>
+    
+            
+
+
+          
+          
+
+
+
+
+
+
+
+
+
+
+
+            
+        </div>
+    </div>
+</div>
+<?php
+include "footer.php";
+?>
--- footer.php
+++ footer.php
@@ -1,26 +1,143 @@
-  <!-- footer section -->
-  <footer class=" footer_section">
-      <div class="container" style="text-align: center; padding: 50px;">
-        <p>
-          &copy; <span id="displayYear"></span> All Rights Reserved By
-          <a href="https://html.design/">free template</a>
-        </p>
-      </div>  
-
+     <footer id="footer" class="overflow-hidden">
+      <div class="container">
+        <div class="row">
+          <div class="footer-top-area">
+            <div class="row d-flex flex-wrap justify-content-between">
+              <div class="col-lg-3 col-sm-6 pb-3">
+                <div class="footer-menu">
+                  <img src="images/main-logo.png" alt="logo">
+                  <p>Nisi, purus vitae, ultrices nunc. Sit ac sit suscipit hendrerit. Gravida massa volutpat aenean odio erat nullam 
+                    fringilla.</p>
+                  <div class="social-links">
+                    <ul class="d-flex list-unstyled">
+                      <li>
+                        <a href="#">
+                          <svg class="facebook">
+                            <use xlink:href="#facebook" />
+                          </svg>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#">
+                          <svg class="instagram">
+                            <use xlink:href="#instagram" />
+                          </svg>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#">
+                          <svg class="twitter">
+                            <use xlink:href="#twitter" />
+                          </svg>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#">
+                          <svg class="linkedin">
+                            <use xlink:href="#linkedin" />
+                          </svg>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#">
+                          <svg class="youtube">
+                            <use xlink:href="#youtube" />
+                          </svg>
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-2 col-sm-6 pb-3">
+                <div class="footer-menu text-uppercase">
+                  <h5 class="widget-title pb-2">Quick Links</h5>
+                  <ul class="menu-list list-unstyled text-uppercase">
+                    <li class="menu-item pb-2">
+                      <a href="index.php">Home</a>
+                    </li>
+                    
+                    <li class="menu-item pb-2">
+                      <a href="blog.php">Blogs</a>
+                    </li>
+                    <li class="menu-item pb-2">
+                      <a href="contact.php">Contact</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div class="col-lg-3 col-sm-6 pb-3">
+                <div class="footer-menu text-uppercase">
+                  <h5 class="widget-title pb-2">Help & Info Help</h5>
+                  <ul class="menu-list list-unstyled">
+                    <li class="menu-item pb-2">
+                      <a href="#">Track Your Order</a>
+                    </li>
+                    <li class="menu-item pb-2">
+                      <a href="#">Returns Policies</a>
+                    </li>
+                    <li class="menu-item pb-2">
+                      <a href="#">Shipping + Delivery</a>
+                    </li>
+                    <li class="menu-item pb-2">
+                      <a href="#">Contact Us</a>
+                    </li>
+                    <li class="menu-item pb-2">
+                      <a href="#">Faqs</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+               <div class="col-lg-3 col-sm-6 pb-3">
+                <div class="footer-menu contact-item">
+                  <h5 class="widget-title text-uppercase pb-2">Contact Us</h5>
+                  <p>Do you have any queries or suggestions? <a href="mailto:">yourinfo@gmail.com</a>
+                  </p>
+                  <p>If you need support? Just give us a call. <a href="">+55 111 222 333 44</a>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <hr>
     </footer>
-    <!-- footer section -->
-
-  </section>
-
-  <!-- end info section -->
-
-
-  <script src="js/jquery-3.4.1.min.js"></script>
-  <script src="js/bootstrap.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js">
-  </script>
-  <script src="js/custom.js"></script>
-
-</body>
-
-</html>
+     <div id="footer-bottom">
+      <div class="container">
+        <div class="row d-flex flex-wrap justify-content-between">
+          <div class="col-md-4 col-sm-6">
+            <div class="Shipping d-flex">
+              <p>We ship with:</p>
+              <div class="card-wrap ps-2">
+                <img src="images/dhl.png" alt="visa">
+                <img src="images/shippingcard.png" alt="mastercard">
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-6">
+            <div class="payment-method d-flex">
+              <p>Payment options:</p>
+              <div class="card-wrap ps-2">
+                <img src="images/visa.jpg" alt="visa">
+                <img src="images/mastercard.jpg" alt="mastercard">
+                <img src="images/paypal.jpg" alt="paypal">
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-6">
+            <div class="copyright">
+              <p>© Copyright 2023 MiniStore. Design by <a href="https://templatesjungle.com/">TemplatesJungle</a> Distribution by <a href="https://themewagon.com">ThemeWagon</a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <script src="js/jquery-1.11.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.bundle.min.js"></script>
+    <script type="text/javascript" src="js/plugins.js"></script>
+    <script type="text/javascript" src="js/script.js"></script>
+  </body> 
+  </html>
