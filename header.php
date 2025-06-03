6ce4b83506ab3dbdbdd8f2d4f4efdc0a5ba12dba
= <=>
2025-06-03 14:38:26 -0700

    same change

--- header.php
+++ header.php
@@ -37,6 +37,13 @@ https://templatemo.com/tm-591-villa-agency
 -->
 
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+  <style>
+    .navbar-brand img { height: 50px; }
+    .navbar { background: #ffb6c1; }
+    .navbar-nav .nav-link { font-size: 1.1rem; margin-right: 30px; }
+    .navbar-nav .dropdown-menu { background: #fff0f5; }
+    .shop-title { font-family: 'Poppins', sans-serif; font-size: 2.5rem; font-weight: 700; color: #d63384; }
+  </style>
 </head>
 
 <body>
@@ -54,72 +61,40 @@ https://templatemo.com/tm-591-villa-agency
   </div>
   <!-- ***** Preloader End ***** -->
 
-  <div class="sub-header">
+  <!-- Header -->
+  <nav class="navbar navbar-expand-lg navbar-light shadow-sm">
     <div class="container">
-      <div class="row">
-        <div class="col-lg-8 col-md-8">
-          <ul class="info">
-            <!-- <li><i class="fa fa-envelope"></i> info@company.com</li> -->
-            <!-- <li><i class="fa fa-map"></i> Sunny Isles Beach, FL 33160</li> -->
-          </ul>
-        </div>
-        <!-- <div class="col-lg-4 col-md-4">
-          <ul class="social-links">
-            <li><a href="#"><i class="fab fa-facebook"></i></a></li>
-            <li><a href="https://x.com/minthu" target="_blank"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="#"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="#"><i class="fab fa-instagram"></i></a></li>
-          </ul>
-        </div> -->
+      <a class="navbar-brand d-flex align-items-center" href="index.php">
+        <img src="logo1.png" alt="Logo" class="me-2">
+        <span class="shop-title">Grocery Shop</span>
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainNavbar" aria-controls="mainNavbar" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="mainNavbar">
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+          <li class="nav-item"><a class="nav-link active" href="index.php">Home</a></li>
+          <li class="nav-item"><a class="nav-link" href="about.php">About Us</a></li>
+          <li class="nav-item"><a class="nav-link" href="Services.php">Services</a></li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="servicesDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Services Details
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="servicesDropdown">
+              <li><a class="dropdown-item" href="beauty.php">Beauty and Hygiene</a></li>
+              <li><a class="dropdown-item" href="baby_care.php">Baby Care</a></li>
+              <li><a class="dropdown-item" href="cleaning.php">Cleaning and Householding</a></li>
+              <li><a class="dropdown-item" href="grains.php">Grains</a></li>
+            </ul>
+          </li>
+          <li class="nav-item"><a class="nav-link" href="privacy.php">Privacy Policy</a></li>
+          <li class="nav-item"><a class="nav-link" href="Contact.php">Contact Us</a></li>
+        </ul>
       </div>
     </div>
-  </div>
-  <br><br>
-  <!-- ***** Header Area Start ***** -->
-  <header class="header-area header-sticky">
-    <div class="container">
-      <div class="row">
-        <div class="col-12">
-          <nav class="main-nav">
-            <!-- ***** Logo Start ***** -->
-            <a href="index.php" class="logo">
-             
-            </a>
-            <br><br>
-            <!-- ***** Logo End ***** -->
-            <!-- ***** Menu Start ***** -->
+  </nav>
+  <!-- End Header -->
 
-            <div class="container">
-              <div class="row">
-                <div class="col-sm-2" style="background-color: white; height:13px;"><img src="logo1.png" alt=""></div>
-                
-                <div class="col-md-10" style="height:50px; font-size:50px;" ><h1 style="font-size: 70px; text-align:center;"> Grocerry Shop</h1></div>
-              </div>
-            </div><br><br><br>
-            <br><br><br><br>
-
-            <div class="card" style="background-color: pink; height: 70px;">
-              <div class="card-header"  style="background-color: pink; height: 70px;">
-
-                <ul class="nav">
-                  <li style="margin-right: 90px;"><a href="index.php" class="active">Home</a></li>
-                  <li style="margin-right: 90px;"><a href="about.php">About us</a></li>
-                  <li style="margin-right: 90px;"><a href="Services.php">Services</a></li>
-                  <div class="dropdown">
-                    <button class="btn btn-secondary dropdown-toggle"  style="margin-right: 90px; background-color: pink;" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                      Services Details
-                    </button>
-                    <ul class="dropdown-menu">
-                      <li><a class="dropdown-item" href="beauty.php">Beauty and hygiene</a></li>
-                      <li><a class="dropdown-item" href="baby_care.php">Baby Care</a></li>
-                      <li><a class="dropdown-item" href="cleaning.php">Cleaning and Householiding</a></li>
-                      <li><a class="dropdown-item" href="grains.php">Grains</a></li>
-
-
-                    </ul>
-                  </div>
-                  <li style="margin-right: 90px;"><a href="privacy.php">Privacy and Police</a></li>
-                  <li style="margin-right: 100px;"><a href="Contact.php">Contact us</a></li>
-                </ul>
-              </div>
-            </div>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
